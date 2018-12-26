3ed064806cbca9097875231425c177e0c809bc87
ysj <ysjr-2002@163.com>
2018-12-26 16:31:36 +0800

    Merge branch 'master' of https://github.com/ysjr-2002/DaggerStudy
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	facetv/build.gradle

--- facetv/src/main/java/com/pa/door/facetv/util/IPUtil.java
+++ facetv/src/main/java/com/pa/door/facetv/util/IPUtil.java
@@ -1,6 +1,8 @@
 package com.pa.door.facetv.util;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
@@ -61,6 +63,53 @@ public class IPUtil {
         }
         return hostIp;
     }
+
+    public static String getWifiIP(Context context) {
+
+        NetworkInfo info = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if (info != null && info.isConnected()) {
+            if (info.getType() == ConnectivityManager.TYPE_WIFI) {
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                String ipAddress = intIP2StringIP(wifiInfo.getIpAddress());//得到IPV4地址
+                return ipAddress;
+            }
+
+            if (info.getType() == ConnectivityManager.TYPE_ETHERNET) {
+                return getLocalIp();
+            }
+        }
+
+        return "";
+    }
+
+    private static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
+    }
+
+    private static String getLocalIp() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()
+                            && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+
+        }
+        return "0.0.0.0";
+    }
+
 
     public static String getIP2(Context context) {
 
@@ -165,5 +214,4 @@ public class IPUtil {
         }
         return fileData.toString();
     }
-
 }
